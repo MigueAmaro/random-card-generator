6fd5642b7991ee1c43c6e7b4ebcc4eee4814e5cb
Miguel Amaro <magenio@icloud.com>
2022-03-15 18:18:50 -0400

    Small fixes. center the dealer class using flex instead

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 window.onload = () => {
     let body = document.querySelector("#body");
-    body.style.backgroundColor = "green"
+    body.style.backgroundColor = "green";
+    body.style.margin = "0";
+    body.style.height = "100vh";
+    body.style.display = "flex";
+    body.style.justifyContent = "center";
+    body.style.alignItems = "center";
 
     let randomNumber = () => {
         const numbers = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 'K', 'J', 'Q'];
@@ -15,42 +20,42 @@ window.onload = () => {
         return symbols[getRandomSymbols];
     }
 
-    var symbolGenerated = randomSymbol();  
+    var symbolGenerated = randomSymbol();
 
     let dealer = document.querySelector("#dealer");
-    dealer.style.width = "350px";
-    dealer.style.height = "500px";
+    dealer.style.width = "400px";
+    dealer.style.height = "600px";
     dealer.style.backgroundColor = "white";
-    dealer.style.margin = "3rem 30%";
     dealer.style.borderRadius = "20px";
     dealer.style.display = "flex";
+    dealer.style.flexDirection = "column";
     dealer.style.justifyContent = "space-between";
-    dealer.style.position = "absolute";
     dealer.style.border = "1px solid black";
     if (symbolGenerated == '♥' || symbolGenerated == '♦') {
         dealer.style.color = "red";
-      } else {
+    } else {
         dealer.style.color = "black";
-      }
+    }
 
-    let symbol1 = document.createElement("symbol1");
+    let symbol1 = document.createElement("span");
     symbol1.style.alignSelf = "start";
     symbol1.style.fontSize = "160px";
     symbol1.style.paddingLeft = "20px";
     symbol1.innerText = `${symbolGenerated}`;
     dealer.appendChild(symbol1);
 
-    let number = document.createElement("number");
+    let number = document.createElement("span");
     number.style.alignItems = "center";
-    number.style.fontSize = "200px";
+    number.style.fontSize = "180px";
     number.style.alignSelf = "center";
     number.innerText = `${randomNumber()}`;
     dealer.appendChild(number);
 
-    let symbol2 = document.createElement("symbol2");
+    let symbol2 = document.createElement("span");
     symbol2.style.fontSize = "160px";
     symbol2.style.alignSelf = "end";
     symbol2.style.paddingRight = "25px";
+    symbol2.style.paddingBottom = "30px";
     symbol2.innerText = `${symbolGenerated}`;
     dealer.appendChild(symbol2);
 }
